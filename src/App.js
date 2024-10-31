243bb3082fcc7127b0a237a191e220f9b9a94b4d
Harugo Shintani <shin@shin-MBA.local>
2024-10-31 19:46:06 +0900

    add-apiClient_getUser

--- src/App.js
+++ src/App.js
@@ -6,20 +6,38 @@ import SignIn from "./pages/SignIn";
 import UserPage from "./pages/UserPage";
 import NewPostPage from "./pages/NewPostPage";
 import UserSettingsPage from "./pages/UserSettingsPage";
+import { UserProvider } from './UserContext';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/user/edit" element={<UserSettingsPage />} />
-        <Route path="/new/post" element={<NewPostPage />} />
+        <Route path="/user" element={<UserProvider><UserPage /></UserProvider>} />
+        <Route path="/user/edit" element={<UserProvider><UserSettingsPage /></UserProvider>} />
+        <Route path="/new/post" element={<UserProvider><NewPostPage /></UserProvider>} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signin" element={<UserProvider><SignIn /></UserProvider>} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
+// const App = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/user" element={<UserPage />} />
+//         <Route path="/user/edit" element={<UserSettingsPage />} />
+//         <Route path="/new/post" element={<NewPostPage />} />
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/signin" element={<SignIn />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
---
+++ src/UserContext.js
@@ -0,0 +1,16 @@
+// UserId　を管理するコンテキスト
+import React, { createContext, useContext, useState } from 'react';
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [userId, setUserId] = useState(null);
+  
+  return (
+    <UserContext.Provider value={{ userId, setUserId }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUserContext = () => useContext(UserContext);
